0b7e4ccab4f68629eb78b6a727b63d6d1c77e06a
Abheek Tripathy <abheek.tripathy@gmail.com>
2023-04-21 15:47:43 +0530

    deploying up

--- script/helloWorld.s.sol
+++ script/helloWorld.s.sol
@@ -4,14 +4,15 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import {HelloWorld} from "../src/helloworld.sol";
 
-
 //make sure you refer the right contract in the cli command to deploy
 contract ContractScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
         new HelloWorld("Foundry is fast!");
-        
+        vm.stopBroadcast();
     }
 }
