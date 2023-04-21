30473c438c215b7648ecb243f4310387d44e51e8
Abheek Tripathy <abheek.tripathy@gmail.com>
2023-04-21 14:26:49 +0530

    contracts,tests init

---
+++ script/helloWorld.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import {HelloWorld} from "../src/helloworld.sol";
+
+
+//make sure you refer the right contract in the cli command to deploy
+contract ContractScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.broadcast();
+        new HelloWorld("Foundry is fast!");
+        
+    }
+}
